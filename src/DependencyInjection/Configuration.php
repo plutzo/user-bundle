0fd8e0f8dbbe9acde86448fad58b21ff561c5a34
Elias Müller <em@aha.biz>
2018-07-03 13:12:32 +0200

    Adapted bundle config / DI.

---
+++ src/DependencyInjection/Configuration.php
@@ -0,0 +1,69 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Marlinc\UserBundle\DependencyInjection;
+
+use Sonata\UserBundle\Admin\Entity\GroupAdmin;
+use Sonata\UserBundle\Admin\Entity\UserAdmin;
+use Sonata\UserBundle\Entity\BaseGroup;
+use Sonata\UserBundle\Entity\BaseUser;
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+/**
+ * This is the class that validates and merges configuration from your app/config files.
+ *
+ * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html#cookbook-bundles-extension-config-class}
+ */
+class Configuration implements ConfigurationInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getConfigTreeBuilder()
+    {
+        $treeBuilder = new TreeBuilder();
+        $rootNode = $treeBuilder->root('marlinc_user');
+
+        $rootNode
+            ->children()
+                ->scalarNode('impersonating_route')->end()
+                ->arrayNode('impersonating')
+                    ->children()
+                        ->scalarNode('route')->defaultFalse()->end()
+                        ->arrayNode('parameters')
+                            ->useAttributeAsKey('id')
+                            ->prototype('scalar')->end()
+                        ->end()
+                    ->end()
+                ->end()
+                ->arrayNode('google_authenticator')
+                    ->addDefaultsIfNotSet()
+                    ->children()
+                        ->scalarNode('server')->cannotBeEmpty()->end()
+                        ->scalarNode('enabled')->defaultFalse()->end()
+                        ->arrayNode('ip_white_list')
+                            ->prototype('scalar')->end()
+                            ->defaultValue(['127.0.0.1'])
+                            ->info('IPs for which 2FA will be skipped.')
+                        ->end()
+                        ->arrayNode('forced_for_role')
+                            ->prototype('scalar')->end()
+                            ->defaultValue(['ROLE_ADMIN'])
+                            ->info('User roles for which 2FA is necessary.')
+                        ->end()
+                    ->end()
+                ->end()
+                ->arrayNode('profile')
+                    ->addDefaultsIfNotSet()
+                    ->children()
+                        ->scalarNode('default_avatar')->defaultValue('bundles/marlincuser/default_avatar.png')->end()
+                    ->end()
+                ->end()
+            ->end()
+        ;
+
+        return $treeBuilder;
+    }
+}
--- src/DependencyInjection/MarlincUserExtension.php
+++ src/DependencyInjection/MarlincUserExtension.php
@@ -8,6 +8,7 @@
 
 namespace Marlinc\UserBundle\DependencyInjection;
 
+use Symfony\Component\Config\Definition\Processor;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
@@ -15,13 +16,100 @@ use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 
 class MarlincUserExtension extends Extension
 {
+    /**
+     * {@inheritdoc}
+     */
+    public function prepend(ContainerBuilder $container): void
+    {
+        if ($container->hasExtension('twig')) {
+            // add custom form widgets
+            $container->prependExtensionConfig('twig', ['form_themes' => ['@MarlincUser/Form/form_admin_fields.html.twig']]);
+        }
+    }
+
+    /**
+     * @inheritdoc
+     */
     public function load(array $configs, ContainerBuilder $container)
     {
+        $processor = new Processor();
+        $configuration = new Configuration();
+        $config = $processor->processConfiguration($configuration, $configs);
+        $config = $this->fixImpersonating($config);
+
         $loader = new XmlFileLoader(
             $container,
             new FileLocator(__DIR__ . '/../Resources/config')
         );
         $loader->load('services.xml');
         $loader->load('admin.xml');
+
+        if (class_exists('Google\Authenticator\GoogleAuthenticator')) {
+            $loader->load('google_authenticator.xml');
+        }
+
+        $container->setParameter('sonata.user.default_avatar', $config['profile']['default_avatar']);
+        $container->setParameter('sonata.user.impersonating', $config['impersonating']);
+
+        $this->configureGoogleAuthenticator($config, $container);
+    }
+
+    /**
+     * @param array $config
+     * @throws \RuntimeException
+     * @return array
+     */
+    public function fixImpersonating(array $config)
+    {
+        if (isset($config['impersonating'], $config['impersonating_route'])) {
+            throw new \RuntimeException('you can\'t have `impersonating` and `impersonating_route` keys defined at the same time');
+        }
+
+        if (isset($config['impersonating_route'])) {
+            $config['impersonating'] = [
+                'route' => $config['impersonating_route'],
+                'parameters' => [],
+            ];
+        }
+
+        if (!isset($config['impersonating']['parameters'])) {
+            $config['impersonating']['parameters'] = [];
+        }
+
+        if (!isset($config['impersonating']['route'])) {
+            $config['impersonating'] = false;
+        }
+
+        return $config;
+    }
+
+    /**
+     * @param array $config
+     * @param ContainerBuilder $container
+     * @throws \RuntimeException
+     * @return mixed
+     */
+    public function configureGoogleAuthenticator($config, ContainerBuilder $container)
+    {
+        $container->setParameter('marlinc.user.google.authenticator.enabled', $config['google_authenticator']['enabled']);
+
+        if (!$config['google_authenticator']['enabled']) {
+            $container->removeDefinition('marlinc.user.google.authenticator');
+            $container->removeDefinition('marlinc.user.google.authenticator.provider');
+            $container->removeDefinition('marlinc.user.google.authenticator.interactive_login_listener');
+            $container->removeDefinition('marlinc.user.google.authenticator.request_listener');
+
+            return;
+        }
+
+        if (!class_exists('Google\Authenticator\GoogleAuthenticator')) {
+            throw new \RuntimeException('Please add ``sonata-project/google-authenticator`` package');
+        }
+
+        $container->setParameter('marlinc.user.google.authenticator.forced_for_role', $config['google_authenticator']['forced_for_role']);
+        $container->setParameter('marlinc.user.google.authenticator.ip_white_list', $config['google_authenticator']['ip_white_list']);
+
+        $container->getDefinition('marlinc.user.google.authenticator.provider')
+            ->replaceArgument(0, $config['google_authenticator']['server']);
     }
 }
