f81995d286c409bab022f4f484ac105829e0768c
Elias Müller <em@aha.biz>
2023-02-16 10:24:04 +0100

    Register basic user checker as a service.

--- src/Resources/config/security.php
+++ src/Resources/config/security.php
@@ -2,6 +2,7 @@
 
 declare(strict_types=1);
 
+use Marlinc\UserBundle\Security\UserChecker;
 use Marlinc\UserBundle\Security\UserProvider;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use function Symfony\Component\DependencyInjection\Loader\Configurator\service;
@@ -11,5 +12,6 @@ return static function (ContainerConfigurator $containerConfigurator): void {
         ->set('marlinc.user.security.user_provider', UserProvider::class)
             ->args([
                 service('marlinc.user.manager.user'),
-            ]);
+            ])
+        ->set(UserChecker::class);
 };
